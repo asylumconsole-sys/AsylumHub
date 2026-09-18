ff00cc43210632672feae165211e8fbfbaa56c18
asylumconsole-sys <asylumconsole@gmail.com>
2026-09-18 14:41:08 +0200

    feat(item-shop): full CE catalog, spawn backend, buy+spawn UI (#4)
    * feat(item-shop): catalog loader, CE item spawn/delivery, buy+spawn UI
    
    * feat(item-shop): update catalog loader for 9 catalog parts
    
    * feat(item-shop): catalog part-08
    
    * feat(item-shop): compact 8-part catalog loader
    
    * feat(item-shop): catalog data part 15
    
    * feat(item-shop): 16-part catalog loader
    
    * feat(item-shop): catalog data parts 3 and 13
    
    * feat(item-shop): catalog data parts 11 and 12
    
    * feat(item-shop): catalog data part 14
    
    * feat(item-shop): catalog data parts 2 and 10
    
    * feat(item-shop): catalog data parts 8 and 9
    
    * feat(item-shop): catalog data part 4
    
    * feat(item-shop): catalog data part 7
    
    * feat(item-shop): catalog data parts 5 and 6
    
    * feat(item-shop): catalog data part 0
    
    * feat(item-shop): catalog data part 1
    
    * feat(item-shop): thin route + ItemImage helpers
    
    * feat(item-shop): ItemShopContent buy+spawn UI
    
    * chore(item-shop): remove obsolete catalog-parts remnant

---
+++ src/lib/dayz/item-catalog.ts
@@ -0,0 +1,82 @@
+import { ITEM_CATALOG_COMPACT_0 } from "./item-catalog-data-0";
+import { ITEM_CATALOG_COMPACT_1 } from "./item-catalog-data-1";
+import { ITEM_CATALOG_COMPACT_2 } from "./item-catalog-data-2";
+import { ITEM_CATALOG_COMPACT_3 } from "./item-catalog-data-3";
+import { ITEM_CATALOG_COMPACT_4 } from "./item-catalog-data-4";
+import { ITEM_CATALOG_COMPACT_5 } from "./item-catalog-data-5";
+import { ITEM_CATALOG_COMPACT_6 } from "./item-catalog-data-6";
+import { ITEM_CATALOG_COMPACT_7 } from "./item-catalog-data-7";
+import { ITEM_CATALOG_COMPACT_8 } from "./item-catalog-data-8";
+import { ITEM_CATALOG_COMPACT_9 } from "./item-catalog-data-9";
+import { ITEM_CATALOG_COMPACT_10 } from "./item-catalog-data-10";
+import { ITEM_CATALOG_COMPACT_11 } from "./item-catalog-data-11";
+import { ITEM_CATALOG_COMPACT_12 } from "./item-catalog-data-12";
+import { ITEM_CATALOG_COMPACT_13 } from "./item-catalog-data-13";
+import { ITEM_CATALOG_COMPACT_14 } from "./item-catalog-data-14";
+import { ITEM_CATALOG_COMPACT_15 } from "./item-catalog-data-15";
+
+const CATEGORY_BY_INDEX = [
+  "Weapons",
+  "Medical",
+  "Tools",
+  "Building",
+  "Food & Drink",
+  "Ammo & Mags",
+  "Clothing",
+  "Containers",
+  "Explosives",
+  "Misc",
+] as const;
+
+export type ShopCatalogItem = {
+  id: string;
+  classname: string;
+  name: string;
+  category: string;
+  price: number;
+  image: string;
+  description: string;
+};
+
+const SEP = "\u001f";
+
+function parseCompact(block: string): ShopCatalogItem[] {
+  return block.split("\n").filter(Boolean).map((line) => {
+    const [classname, name, priceStr, catIdx] = line.split(SEP);
+    const category = CATEGORY_BY_INDEX[Number(catIdx)] ?? "Misc";
+    return {
+      id: classname,
+      classname,
+      name,
+      category,
+      price: Number(priceStr),
+      image: `https://dayz.wiki.gg/images/${classname}.png`,
+      description: category,
+    };
+  });
+}
+
+export const ITEM_CATALOG: ShopCatalogItem[] = [
+  ...parseCompact(ITEM_CATALOG_COMPACT_0),
+  ...parseCompact(ITEM_CATALOG_COMPACT_1),
+  ...parseCompact(ITEM_CATALOG_COMPACT_2),
+  ...parseCompact(ITEM_CATALOG_COMPACT_3),
+  ...parseCompact(ITEM_CATALOG_COMPACT_4),
+  ...parseCompact(ITEM_CATALOG_COMPACT_5),
+  ...parseCompact(ITEM_CATALOG_COMPACT_6),
+  ...parseCompact(ITEM_CATALOG_COMPACT_7),
+  ...parseCompact(ITEM_CATALOG_COMPACT_8),
+  ...parseCompact(ITEM_CATALOG_COMPACT_9),
+  ...parseCompact(ITEM_CATALOG_COMPACT_10),
+  ...parseCompact(ITEM_CATALOG_COMPACT_11),
+  ...parseCompact(ITEM_CATALOG_COMPACT_12),
+  ...parseCompact(ITEM_CATALOG_COMPACT_13),
+  ...parseCompact(ITEM_CATALOG_COMPACT_14),
+  ...parseCompact(ITEM_CATALOG_COMPACT_15),
+];
+
+export const ITEM_CATEGORIES: string[] = [...CATEGORY_BY_INDEX];
+
+export function getCatalogItem(id: string): ShopCatalogItem | undefined {
+  return ITEM_CATALOG.find((item) => item.id === id);
+}
