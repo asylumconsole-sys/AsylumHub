712dfbf64f889a9809b9a9e61965e3984705dfb0
asylumconsole-sys <asylumconsole@gmail.com>
2026-09-19 15:35:43 +0200

    fix(npc-shop): match item-shop export order for Rollup

--- src/routes/_app/tools/npc-shop.tsx
+++ src/routes/_app/tools/npc-shop.tsx
@@ -1,8 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { NPCShopContent } from "./npc-shop-content";
 
-export { NPCShopContent };
-
 export const Route = createFileRoute("/_app/tools/npc-shop")({
   component: NPCShopContent,
 });
+
+export { NPCShopContent };
