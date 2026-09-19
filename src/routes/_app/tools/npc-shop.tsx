b27e4e275df92013f5e622cecb15c125e9dc3a69
asylumconsole-sys <asylumconsole@gmail.com>
2026-09-19 15:25:56 +0200

    fix(npc-shop): restore pack tiers, charges UI, Beamer View inventory

--- src/routes/_app/tools/npc-shop.tsx
+++ src/routes/_app/tools/npc-shop.tsx
@@ -1,1 +1,6 @@
-PLACEHOLDER_LOADING
+import { createFileRoute } from "@tanstack/react-router";
+import { NPCShopContent } from "./npc-shop-content";
+
+export const Route = createFileRoute("/_app/tools/npc-shop")({
+  component: NPCShopContent,
+});
