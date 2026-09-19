c3e50344991c929868f3f9d7515672bda3b77306
asylumconsole-sys <asylumconsole@gmail.com>
2026-09-19 15:09:43 +0200

    chore(npc): extract Beamer cfgSpawnableTypes

---
+++ src/lib/npc/the-beamer-cfg.ts
@@ -0,0 +1,114 @@
+/** Beamer cfgSpawnableTypes payload. */
+export const THE_BEAMER_CFG_SPAWNABLETYPES = `<type name="TheBeamer">
+  <attachments>
+    <item name="HandcuffsLocked"/>
+  </attachments>
+  <attachments>
+    <item name="M14">
+      <attachments>
+        <item name="Mag_DMR_20Rnd"/>
+        <item name="MK4Optic_Green"/>
+        <item name="ImprovisedSuppressor"/>
+      </attachments>
+    </item>
+  </attachments>
+  <attachments>
+    <item name="M4A1_Green">
+      <attachments>
+        <item name="Mag_STANAG_60Rnd"/>
+        <item name="BarakaOptic"/>
+        <item name="M4_Suppressor"/>
+        <item name="M4_CQBBttstck_Green"/>
+        <item name="M4_PlasticHndgrd_Green"/>
+      </attachments>
+    </item>
+  </attachments>
+  <attachments>
+    <item name="Mich2001Helmet">
+      <attachments>
+        <item name="NVGHeadstrap"/>
+        <item name="Battery9V"/>
+      </attachments>
+    </item>
+  </attachments>
+  <attachments>
+    <item name="HockeyMask"/>
+  </attachments>
+  <attachments>
+    <item name="PlateCarrierVest_Green">
+      <attachments>
+        <item name="PlateCarrierPouches_Green">
+          <cargo>
+            <item name="Mag_STANAG_60Rnd"/>
+            <item name="Mag_STANAG_60Rnd"/>
+            <item name="ImprovisedSuppressor"/>
+          </cargo>
+        </item>
+        <item name="RGD5Grenade"/>
+        <item name="RGD5Grenade"/>
+        <item name="M84Flashbang"/>
+        <item name="M84Flashbang"/>
+      </attachments>
+    </item>
+  </attachments>
+  <attachments>
+    <item name="TShirt_White"/>
+  </attachments>
+  <attachments>
+    <item name="WoolGlovesFingerless_ColorBase"/>
+  </attachments>
+  <attachments>
+    <item name="GorkaPants_Autumn">
+      <cargo>
+        <item name="Box_308WINTR_20rnd"/>
+        <item name="Box_308WINTR_20rnd"/>
+        <item name="Mag_STANAG_60Rnd"/>
+        <item name="Mag_STANAG_60Rnd"/>
+        <item name="SewingKit"/>
+        <item name="Mag_STANAG_60Rnd"/>
+        <item name="SewingKit"/>
+        <item name="Box_556x45Tracer_20rnd"/>
+        <item name="Box_556x45Tracer_20rnd"/>
+        <item name="Bandage"/>
+      </cargo>
+    </item>
+  </attachments>
+  <attachments>
+    <item name="CanvasShoes_Red"/>
+  </attachments>
+  <attachments>
+    <item name="HipPack_Medical">
+      <attachments>
+        <item name="Canteen" quantityMin="1.0" quantityMax="1.0"/>
+        <cargo>
+          <item name="CodeinePills"/>
+          <item name="TetracyclinePills"/>
+          <item name="Morphine"/>
+        </cargo>
+      </attachments>
+    </item>
+  </attachments>
+  <attachments>
+    <item name="AssaultBag_Green">
+      <attachments>
+        <item name="FieldShovel"/>
+        <item name="XmasLights_Red"/>
+        <item name="Rangefinder">
+          <attachments>
+            <item name="Battery9V"/>
+          </attachments>
+        </item>
+      </attachments>
+      <cargo>
+        <item name="WaterBottle" quantityMin="1.0" quantityMax="1.0"/>
+        <item name="PeachesCan"/>
+        <item name="PeachesCan"/>
+        <item name="LandMine"/>
+        <item name="LandMine"/>
+        <item name="CombatKnife"/>
+        <item name="Bandage"/>
+        <item name="WeaponCleaningKit"/>
+      </cargo>
+    </item>
+  </attachments>
+</type>`;
