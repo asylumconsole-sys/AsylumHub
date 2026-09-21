b94f189cef9431d63e6a232cf96e6aab61eb83b0
asylumconsole-sys <asylumconsole@gmail.com>
2026-09-21 21:02:10 +0200

    fix(npc): npc-shop accepts maker search flag

--- src/routes/_app/tools/npc-shop.tsx
+++ src/routes/_app/tools/npc-shop.tsx
@@ -1,8 +1,16 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { z } from "zod";
 import { NPCShopContent } from "./npc-shop-content";
 
 export const Route = createFileRoute("/_app/tools/npc-shop")({
-  component: NPCShopContent,
+  validateSearch: z.object({ maker: z.union([z.boolean(), z.string()]).optional() }),
+  component: NpcShopPage,
 });
+
+function NpcShopPage() {
+  const { maker } = Route.useSearch();
+  const startOnBuilder = maker === true || maker === "1" || maker === "true";
+  return <NPCShopContent startOnBuilder={startOnBuilder} />;
+}
 
 export { NPCShopContent };
