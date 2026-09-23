5e737a1be80feab8e2253386511138b77c7ee326
asylumconsole-sys <asylumconsole@gmail.com>
2026-09-24 01:11:07 +0200

    feat(casino): assembler for embedded lobby painting

--- src/lib/pro-casino.ts
+++ src/lib/pro-casino.ts
@@ -1,1 +1,6 @@
-export const PRO_CASINO_IMG = "/pro-casino.jpg";
+import { C0 } from "./casino-c0";
+import { C1 } from "./casino-c1";
+import { C2 } from "./casino-c2";
+import { C3 } from "./casino-c3";
+
+export const PRO_CASINO_IMG = `data:image/jpeg;base64,${C0}${C1}${C2}${C3}`;
