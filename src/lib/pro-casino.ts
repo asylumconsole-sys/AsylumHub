af4911437947079991888a0d694f03526cc555a7
asylumconsole-sys <asylumconsole@gmail.com>
2026-09-24 01:15:29 +0200

    feat(lobby): embed PRO CASINO painting as data URI

--- src/lib/pro-casino.ts
+++ src/lib/pro-casino.ts
@@ -1,6 +1,1 @@
-import { C0 } from "./casino-c0";
-import { C1 } from "./casino-c1";
-import { C2 } from "./casino-c2";
-import { C3 } from "./casino-c3";
-
-export const PRO_CASINO_IMG = `data:image/jpeg;base64,${C0}${C1}${C2}${C3}`;
+export const PRO_CASINO_IMG = "data:image/jpeg;base64,PLACEHOLDER_CASINO";
